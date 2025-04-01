dd8e686d3879323fcd7b9d83d69a1fa0e95cf9c6
Matheus Ribeiro <matheus.tex02@gmail.com>
2025-04-01 12:25:28 -0300

    loading button on registration button antimation

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Formik , Form, Field, ErrorMessage} from 'formik'
 import * as Yup from 'yup'
 import axios from 'axios'
+import CircularProgress from '@mui/material/CircularProgress';
+
 import { useNavigate } from 'react-router-dom'
 
 function Registration() {
   let navigate = useNavigate()
-
+  const [isLoading, setIsLoading] = useState(false)
   const initialValues = {
     username: "",
     password: ""
@@ -18,12 +20,17 @@ function Registration() {
   })
 
   const onSubmit = (data) => {
+    setIsLoading(true)
     axios.post("https://blog-sever-n5qt.onrender.com/auth", data).then((response) => {
         if(response.data.error){
           alert(response.data.error)
+          setIsLoading(false)
+          return
         }else{
           alert(response.data)
           navigate(`/login`)
+          setIsLoading(false)
+          return
         }
         
       })
@@ -43,7 +50,9 @@ function Registration() {
             <ErrorMessage name="password" component="span"/>
             <Field id="inputCreatePost" type="password" name="password" placeholder="(Your password...)"/>
 
-            <button type='submit'>Register</button>
+            <button type='submit'>
+            {isLoading ? <CircularProgress size={20} color='white' /> : "Register"}
+            </button>
           </Form>
 
         </Formik>
